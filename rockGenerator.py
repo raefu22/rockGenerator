51ed55a5ef7ebf8ea1cf25fa06e2263acb35ad29
Rae Fu <94275037+raefu22@users.noreply.github.com>
2023-12-28 23:58:08 -0500

    UV unwrap and creates a shader
    and error fix on positioning 1st and 2nd rock

--- rockGenerator.py
+++ rockGenerator.py
@@ -38,6 +38,7 @@ def randomfloat():
     
 def locations(curvename, level):
     position = cmds.pointOnCurve(curvename, pr = level, turnOnPercentage = True)
+    print(level)
     return position
     
 #main function    
@@ -53,17 +54,29 @@ def createRock():
         name = inputname + str(x)
         
         #basic rock shape
-        cmds.polyCube(w = random.randint(1,3), d = random.randint(1,3), cuv=4, sd = 3, sh = 3, sw = 3, n = name)
+        width = random.randint(1,3)
+        depth = random.randint(1,3)
+        cmds.polyCube(w = width, d = depth, cuv=4, sd = 3, sh = 3, sw = 3, n = name)
+        
         cmds.select(appendName(name, '.f[4]'), appendName(name, '.f[1]'))
         cmds.move(0, 0, random.uniform(0,1), r=True)
         cmds.select(appendName(name, '.f[22]'), appendName(name, '.f[25]'))
         cmds.move(0, 0, -random.uniform(0,1), r=True)
+        
         cmds.select(appendName(name, '.f[37]'), appendName(name, '.f[40]'))
-        cmds.move(random.uniform(0,0.5), 0, 0, r=True)
-        cmds.select(appendName(name, '.f[49]'), appendName(name, '.f[46]'))
-        cmds.move(-random.uniform(0,1), 0, 0, r=True)
-        cmds.select(appendName(name, '.f[13]'))
-        cmds.move(random.uniform(-0.2,0.2), random.uniform(0,1), random.uniform(-0.2,.2), r=True)
+        if (width == 1 and depth == 1):
+            cmds.move(random.uniform(0,0.04), 0, 0, r=True)
+            cmds.select(appendName(name, '.f[49]'), appendName(name, '.f[46]'))
+            cmds.move(-random.uniform(0,0.03), 0, 0, r=True)
+            cmds.select(appendName(name, '.f[13]'))
+            cmds.move(random.uniform(-0.2,0.2), random.uniform(0,0.03), random.uniform(-0.2,.2), r=True)
+        else:
+            cmds.move(random.uniform(0,0.5), 0, 0, r=True)
+            cmds.select(appendName(name, '.f[49]'), appendName(name, '.f[46]'))
+            cmds.move(-random.uniform(0,1), 0, 0, r=True)
+            cmds.select(appendName(name, '.f[13]'))
+            cmds.move(random.uniform(-0.2,0.2), random.uniform(0,1), random.uniform(-0.2,.2), r=True)
+       
         #tweaks
         cmds.select(appendName(name, '.vtx[15]'))
         cmds.move(random.uniform(-.2,-0.01), 0, random.uniform(-0.2,-0.01), r=True)
@@ -84,8 +97,34 @@ def createRock():
         
         #location
         cmds.select(name)
-        position = locations(curvename, (x-1.0)/num)
+        position = locations(curvename, (x-0.0)/num)
         cmds.move(position[0], position[1], position[2], relative = True)
         
+        #UVs
+        cmds.polyProjection(name+".f[0:53]", md = 'y')
+        
+        edgenums = ['.e[0:2]', '.e[27:29]', '.e[72]', '.e[75:76]', '.e[79:80]', '.e[83]', '.e[37]', '.e[41]', '.e[45]']   
+        edges = []
+        for edge in edgenums:
+            edges.append(appendName(name,edge))
+        
+        cmds.polyMapCut(edges)
+        cmds.select(name + '.f[0:53]')
+        cmds.u3dUnfold(name + '.f[0:53]', ite=1, p=0, bi=1, tf=1, ms=1024, rs=0)
+        cmds.u3dLayout(name+'.f[0:53]', res=256, scl=1, box=[0, 1, 0, 1])
+        
+        #material
+        shader = cmds.shadingNode('aiStandardSurface', asShader = True, n=name + 'shader')
+        
+        cmds.sets(renderable=True, noSurfaceShader= True, empty=True, n= 'aiSurfaceShader' + name + 'SG')
+        cmds.select(name)
+        cmds.hyperShade(assign = 'aiSurfaceShader' + name + 'SG')
+        cmds.connectAttr(name + 'shader.outColor', 'aiSurfaceShader' + name +'SG.surfaceShader', f=True)
+        
+        
+        cmds.shadingNode('rock', asTexture = True, n = name + 'rock')
+        cmds.shadingNode('place3dTexture', asUtility = True, n = name + 'place3dTexture')
+        cmds.connectAttr (name + 'place3dTexture.wim[0]', name + 'rock.pm')
+        cmds.connectAttr(name + 'rock.outColor', name + 'shader.baseColor')
         #smooth
-        cmds.polySmooth(mth=0, sdt=2, ovb=1, ofb=3, ofc=0, ost=0, ocr=0, dv=2, bnr=1, c=1, kb=1, ksb=1, khe=0, kt=1, kmb=1, suv=1, peh=0, sl=1, dpe=1, ps=0.1, ro=1, ch=1)
+        #cmds.polySmooth(mth=0, sdt=2, ovb=1, ofb=3, ofc=0, ost=0, ocr=0, dv=2, bnr=1, c=1, kb=1, ksb=1, khe=0, kt=1, kmb=1, suv=1, peh=0, sl=1, dpe=1, ps=0.1, ro=1, ch=1)
